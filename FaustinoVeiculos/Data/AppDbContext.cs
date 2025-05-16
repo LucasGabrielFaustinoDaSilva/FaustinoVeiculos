c4f1be18ebd6fd5af89ef640d6684feac89b1e10
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-05-15 22:21:02 -0300

    corrigir corescontorleller

---
+++ FaustinoVeiculos/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using FaustinoVeiculos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaustinoVeiculos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Cor> Cores { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Modelo> Modelos { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
